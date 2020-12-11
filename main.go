13935715b061cf236c1b21825b8fe18e7be9f83c
Aravind Prasad S <raja.avi@gmail.com>
2020-12-11 11:05:38 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Stock struct {
+	Name         string
+	Actual_value float32
+	Min_value    float32
+	Max_value    float32
+	Email_id     string
+}
+
+var StockData []Stock
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("template.html") //parse the html file homepage.html
+	if err != nil {                                // if there is an error
+		log.Print("template parsing error: ", err) // log it
+	}
+	//fmt.Println(StockData)
+	err = t.Execute(w, StockData) //execute the template with filled data
+	if err != nil {               // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+}
+
+func AddHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	StockName := r.Form.Get("stock-name")
+	MinVal := r.Form.Get("min-value")
+	MaxVal := r.Form.Get("max-value")
+	MailID := r.Form.Get("mail-id")
+
+	MinValFloat, _ := strconv.ParseFloat(MinVal, 32)
+	MaxValFloat, _ := strconv.ParseFloat(MaxVal, 32)
+
+	//fmt.Println(StockName, MinValFloat, MaxValFloat, MailID)
+
+	d := Stock{
+		Name:         StockName,
+		Actual_value: 0,
+		Min_value:    float32(MinValFloat),
+		Max_value:    float32(MaxValFloat),
+		Email_id:     MailID,
+	}
+
+	StockData = append(StockData, d)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	StockName := r.Form.Get("stock-name-del")
+	// fmt.Println(StockName)
+
+	del_index := -1
+	for index, val := range StockData {
+		if val.Name == StockName {
+			fmt.Println("index to be deleted is ", index)
+			del_index = index
+		}
+	}
+	if del_index != -1 {
+		if del_index == 0 {
+			_, StockData = StockData[0], StockData[1:]
+		} else if del_index == len(StockData)-1 {
+			len1 := len(StockData)
+			StockData = StockData[:len1-1]
+		} else {
+			StockData = append(StockData[:del_index], StockData[del_index:]...)
+		}
+	}
+
+	// fmt.Println(StockData)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/new", AddHandler)
+	http.HandleFunc("/delete", DeleteHandler)
+	fmt.Printf("Server started at %d", 8080)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
